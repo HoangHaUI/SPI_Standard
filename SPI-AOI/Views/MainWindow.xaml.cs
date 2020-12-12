a1a488931f51f1a277b4a8c96dac2cbbd13548a9
thieunv <thieunv96@gmail.com>
2020-12-12 18:39:03 +0700

    design main form

--- SPI-AOI/Views/MainWindow.xaml.cs
+++ SPI-AOI/Views/MainWindow.xaml.cs
@@ -24,5 +24,24 @@ namespace SPI_AOI.Views
         {
             InitializeComponent();
         }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            System.Windows.Forms.DataVisualization.Charting.Series
+                series = new System.Windows.Forms.DataVisualization.Charting.Series("YeildRate");
+            series.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            chartYeildRate.Series.Add(series);
+            chartYeildRate.BackColor = System.Drawing.Color.Transparent;
+            chartYeildRate.Series["YeildRate"].Points.AddXY("100%",89);
+            chartYeildRate.Series["YeildRate"].Points.AddXY("30%",22);
+            chartYeildRate.Series["YeildRate"].Points[1].Color = System.Drawing.Color.Red;
+            chartYeildRate.Series["YeildRate"].Points[0].Color = System.Drawing.Color.Green;
+            chartYeildRate.Series["YeildRate"].Points[0].LegendText = "PASS";
+            chartYeildRate.Series["YeildRate"].Points[1].LegendText = "FAIL";
+            chartYeildRate.Titles.Add("Counter Chart");
+            chartYeildRate.BackColor = System.Drawing.Color.Transparent;
+            chartYeildRate.ChartAreas[0].BackColor = System.Drawing.Color.Transparent;
+            chartYeildRate.Legends[0].BackColor = System.Drawing.Color.Transparent;
+        }
     }
 }
